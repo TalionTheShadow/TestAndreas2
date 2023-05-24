95fd8089344fd3e463c8108d34270a7a7d72d3ae
TalionTheShadow <113335240+TalionTheShadow@users.noreply.github.com>
2023-05-24 12:51:46 +0200

    Add files via upload

---
+++ Butterfly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAndreas2
+{
+    internal class Butterfly
+    {
+        public interface IButterfly
+        {
+            void InsertDelete(string choice);
+           
+        }
+
+    }
+}
